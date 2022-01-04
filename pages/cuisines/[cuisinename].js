19467172f2200c0fe2a2ad06de18c2bef173a62d
Junel <sacrojunel@gmail.com>
2022-01-04 19:15:18 +0800

    add cuisine ui/ firebase data

--- pages/cuisines/[cuisinename].js
+++ pages/cuisines/[cuisinename].js
@@ -1,12 +1,98 @@
-import { Box } from "@chakra-ui/react";
-import { useEffect } from "react";
+import Head from "next/head";
+import CuisineType from "../../components/Cuisine";
+import Layout from "../../components/Layout";
+import NavBar from "../../components/NavBar";
+import { Box, Grid, GridItem, Heading, Text } from "@chakra-ui/react";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { db } from "../../lib/firebase.config";
+import { useRouter } from "next/router";
 
-const Cuisine = () => {
+const Cuisine = ({ restaurant }) => {
+  const router = useRouter();
+
   return (
     <>
-      <Box>ok</Box>
+      <Head>
+        <title> Zhacks Foody | {router.query.cuisinename} </title>
+      </Head>
+      <Box as={"main"} pos={"relative"} zIndex={0} px={"20"} py={"10"}>
+        <Heading py={"5"}>
+          Choose the best{" "}
+          <Text as={"span"} color={"red.500"} textTransform={"capitalize"}>
+            burgers
+          </Text>
+        </Heading>
+        <Grid
+          templateColumns={{
+            base: "repeat(1, 1fr)",
+            md: "repeat(2, 1fr)",
+            lg: "repeat(4, 1fr)",
+          }}
+          gap={"5"}
+        >
+          {restaurant.map((burger) => (
+            <GridItem
+              _hover={{ shadow: "2xl", rounded: "md" }}
+              p={"2"}
+              key={burger._id}
+            >
+              <CuisineType data={burger} />
+            </GridItem>
+          ))}
+        </Grid>
+      </Box>
     </>
   );
+};
+
+Cuisine.getLayout = (page) => {
+  return (
+    <Layout>
+      <NavBar />
+      {page}
+    </Layout>
+  );
+};
+
+export const getStaticProps = async (context) => {
+  let q = query(
+    collection(db, "restaurants"),
+    where("tags", "array-contains", context.params.cuisinename)
+  );
+  const querysnapshot = await getDocs(q);
+
+  const restaurant = [];
+  querysnapshot.forEach((doc) => {
+    restaurant.push({ _id: doc.id, ...doc.data() });
+  });
+
+  if (!restaurant) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: { restaurant: restaurant },
+  };
+};
+
+export const getStaticPaths = async () => {
+  const querysnapshot = await getDocs(collection(db, "cuisines"));
+
+  const cuisinedata = [];
+  querysnapshot.forEach((doc) => {
+    cuisinedata.push({ ...doc.data() });
+  });
+
+  const paths = cuisinedata.map((item) => ({
+    params: { cuisinename: item.slug },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
 };
 
 export default Cuisine;
