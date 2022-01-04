c07a5e4e4838ffd8f2724e556a470c7ef999af2b
Junel <sacrojunel@gmail.com>
2022-01-04 19:16:25 +0800

    add homelayout/fetch data from firestore

--- pages/index.js
+++ pages/index.js
@@ -2,36 +2,48 @@ import { Box } from "@chakra-ui/react";
 import Head from "next/head";
 import FoodCategory from "../components/FoodCategory";
 import HeroSection from "../components/HeroSection";
+import Layout from "../components/Layout";
+import NavbarHome from "../components/NavbarHome";
+//import { cuisines } from "../data/cuisines";
+import { collection, doc, getDocs, getFirestore } from "firebase/firestore";
+import { db } from "../lib/firebase.config";
 
-export default function Home() {
+export default function Home({ cuisines }) {
   return (
     <>
       <Head>
         <title>Zhacks Foody</title>
-
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
+        <meta
+          name="description"
+          content="Choose food that you  like , here in Zhack's foody"
         />
       </Head>
 
       <HeroSection />
       <Box id="food cuisine" pos={"relative"} h={"auto"}>
-        <FoodCategory />
+        <FoodCategory data={cuisines} />
       </Box>
     </>
   );
 }
+
+Home.getLayout = (page) => {
+  return (
+    <Layout>
+      <NavbarHome />
+      {page}
+    </Layout>
+  );
+};
+
+export const getStaticProps = async () => {
+  const querysnapshot = await getDocs(collection(db, "cuisines"));
+
+  const cuisinesData = [];
+  querysnapshot.forEach((doc) => {
+    cuisinesData.push({ _id: doc.id, ...doc.data() });
+  });
+  return {
+    props: { cuisines: cuisinesData },
+  };
+};
