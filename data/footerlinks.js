abda53ba3aa88cbd2c8e123a7d3607540973ee47
Junel <sacrojunel@gmail.com>
2022-01-06 01:16:00 +0800

    add footer links

---
+++ data/footerlinks.js
@@ -0,0 +1,52 @@
+export const cuisines = [
+  {
+    title: "Chinese Food",
+    path: "cuisines/chinese",
+  },
+  {
+    title: "Filipino Food",
+    path: "cuisines/filipino",
+  },
+  {
+    title: "Japanese Food",
+    path: "cuisines/japanese",
+  },
+  {
+    title: "Korean Food",
+    path: "cuisines/korean",
+  },
+  {
+    title: "Indian Food",
+    path: "cuisines/indian",
+  },
+];
+
+export const aboutCompany = [
+  {
+    title: "Who We Are",
+    path: "who-we-are",
+  },
+  {
+    title: "Careers",
+    path: "careers",
+  },
+  {
+    title: "Blog",
+    path: "blog",
+  },
+];
+
+export const forYou = [
+  {
+    title: "FAQ",
+    path: "faq",
+  },
+  {
+    title: "Privacy",
+    path: "privacy",
+  },
+  {
+    title: "Terms",
+    path: "terms",
+  },
+];
