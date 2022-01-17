a284333b061e92c5bc22658c07578ac0848ae388
Junel <sacrojunel@gmail.com>
2022-01-17 22:26:46 +0800

    feat: add login page

---
+++ src/pages/login.js
@@ -0,0 +1,13 @@
+import { Box } from "@chakra-ui/react";
+
+const login = () => {
+  return (
+    <>
+      <Box h={{ base: "container.sm", md: "container.md", lg: "container.lg" }}>
+        ok
+      </Box>
+    </>
+  );
+};
+
+export default login;
