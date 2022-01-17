3d5311a02a9ef8017f9c90c3c2a2cecc10821744
Junel <sacrojunel@gmail.com>
2022-01-17 16:42:13 +0800

    chore: dummy data

--- data/menu.js
+++ data/menu.js
@@ -13,4 +13,55 @@ export const menuTitle = [
     id: 4,
     menu: "Fries",
   },
+  {
+    id: 5,
+    menu: "Fries",
+  },
+  {
+    id: 6,
+    menu: "Family Bundles",
+  },
+  {
+    id: 7,
+    menu: "Milk Tea",
+  },
+  // {
+  //   id: 8,
+  //   menu: "Chicken Feet",
+  // },
+  // {
+  //   id: 9,
+  //   menu: "Coffie",
+  // },
+  // {
+  //   id: 10,
+  //   menu: "Dessert",
+  // },
+];
+
+export const menu = [
+  {
+    id: 1,
+    name: "Pork BBQ Buddy Size",
+    desc: "4 sticks Pork BBQ",
+    image: "/images/chicken-food.jpg",
+  },
+  {
+    id: 2,
+    name: "2 Sticks Pork BBQ (Solo)",
+    desc: "2 sticks Pork BBQ with peanut sauce and 1 java rice",
+    image: "/images/chinese-food.jpg",
+  },
+  {
+    id: 3,
+    name: "Mang Inasal - Edsa Central : 1 Stick Pork BBQ (Value Meal)",
+    desc: "1 stick Pork BBQ with peanut sauce and 1 java rice and drink",
+    image: "/images/filipino-food.jpeg",
+  },
+  {
+    id: 4,
+    name: "Chicken Inasal Regular (Value Meal)",
+    desc: "Chicken Inasal Regular with 1 rice and drink, choice of chicken part is subject to availbaility",
+    image: "/images/korean-food.jpg",
+  },
 ];
