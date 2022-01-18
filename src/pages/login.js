a441583c9ffd42f35ab60c28feac1833f9ca6dda
Junel <sacrojunel@gmail.com>
2022-01-18 16:16:40 +0800

    chore; add title

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,8 +1,13 @@
 import { Box } from "@chakra-ui/react";
+import Head from "next/head";
 
 const login = () => {
   return (
     <>
+      <Head>
+        <title>Zhacks Foody | Login</title>
+      </Head>
+
       <Box h={{ base: "container.sm", md: "container.md", lg: "container.lg" }}>
         ok
       </Box>
